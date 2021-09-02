b111e38a969fa8b95ab5f99418f451316906905f
Prajwal Patil <prajwalpatilk@gmail.com>
2021-09-02 12:43:57 +0530

    Add display and insert at front funtions - CSLL

--- lib/circularSinglyLinked.cpp
+++ lib/circularSinglyLinked.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
 #include "../header/singlyNode.h"
+
+void CSLL::display(NODE *&head)
+{
+    std::cout << "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+    std::cout << "Linked List: " << std::endl;
+    if (head == NULL)
+    {
+        std::cout << "The Linked List is empty!";
+        std::cout << "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+        return;
+    }
+    NODE *temp = head;
+    std::cout << "Head -> ";
+    do
+    {
+        temp->displayNode();
+        temp = temp->next;
+    } while (temp->next != head);
+    std::cout << " Head";
+}
+
+void CSLL::insertAtFront(NODE *&head)
+{
+    NODE *n = new NODE();
+    if (head == NULL)
+    {
+        n->next = n;
+        head = n;
+        return;
+    }
+    NODE *temp = head;
+    n->next = temp;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    head = n;
+    temp->next = n;
+}
