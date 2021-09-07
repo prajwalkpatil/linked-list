16665c8e290fe1a60481d225682844210637c162
Prajwal Patil <prajwalpatilk@gmail.com>
2021-09-07 10:07:57 +0530

    Add display function

--- lib/circularDoublyLinked.cpp
+++ lib/circularDoublyLinked.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 #include "../header/doublyNode.h"
+
+void CDLL::display(NODE *&head)
+{
+    std::cout << "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+    std::cout << "Linked List: " << std::endl;
+    if (head == NULL)
+    {
+        std::cout << "The Linked List is empty!";
+        std::cout << "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+        return;
+    }
+    NODE *temp = head;
+    std::cout << "Head -> ";
+    while (temp->next != head)
+    {
+        temp->displayNode();
+        temp = temp->next;
+    }
+    temp->displayNode();
+    std::cout << " Head";
+}
